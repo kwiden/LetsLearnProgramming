3e2fcb91e8a470f6e6893acf4e3bb311c73bcf77
Katie <kt.widen@gmail.com>
2017-12-04 13:04:42 -0600

    Create Component.h

---
+++ Cpp/robot/frc1296code/Component.h
@@ -0,0 +1,41 @@
+
+/** \file
+ * Example of subsystem task declaration.
+ *
+ * This class is derived from the standard Component base class and includes
+ * initialization for the devices used to control a given subsystem.
+ *
+ */
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+/**
+	A template class for creating new components
+ */
+#include <ComponentBase.h>			//For ComponentBase class
+#include <pthread.h>
+#include <string>
+
+//Robot
+#include "WPILib.h"
+
+
+class Component : public ComponentBase
+{
+public:
+	Component();
+	virtual ~Component();
+	static void *StartTask(void *pThis, const char* szComponentName, int iPriority)
+	{
+		pthread_setname_np(pthread_self(), szComponentName);
+		pthread_setschedprio(pthread_self(), iPriority);
+		((Component *)pThis)->DoWork();
+		return(NULL);
+	}
+
+private:
+	void OnStateChange();
+	void Run();
+};
+
+#endif			//COMPONENT_H
